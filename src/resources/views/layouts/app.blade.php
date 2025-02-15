efc88652de12132657cf0c51b1225165e4648912
Ayaka Suzuki <ayakaarskj@gmail.com>
2025-02-15 07:49:11 +0100

    会員登録設定

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>アプリケーション名</title>
+    <title>Contact-form</title>
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
     <link rel="stylesheet" href="{{ asset('css/common.css') }}">
     @yield('css')
@@ -16,19 +16,27 @@
         <div class="header__inner">
             <div class="header-utilities">
                 <a class="header__logo" href="/">
-                    アプリケーション名
+                    FashionablyLate
                 </a>
+
+                <!-- ログインページなら register ボタンを表示 -->
+                @if (Request::is('login'))
+                <a href="/register" class="header__button">register</a>
+                @endif
+
+                <!-- 登録ページなら login ボタンを表示 -->
+                @if (Request::is('register'))
+                <a href="/login" class="header__button">login</a>
+                @endif
+
                 <nav>
                     <ul class="header-nav">
                         @if (Auth::check())
                         <li class="header-nav__item">
-                            <a class="header-nav__link" href="/mypage">マイページ</a>
+                            <a href="/register" class="header-nav__button">Register</a>
                         </li>
                         <li class="header-nav__item">
-                            <form class="form" action="/logout" method="post">
-                                @csrf
-                                <button class="header-nav__button">ログアウト</button>
-                            </form>
+                            <a href="/login" class="header-nav__button">Login</a>
                         </li>
                         @endif
                     </ul>
